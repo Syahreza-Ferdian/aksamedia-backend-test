11e3744a4465917c9cf9485a6af919a8b33f064f
Syahreza-Ferdian <syahrezafistiferdian32@gmail.com>
2025-01-01 14:45:38 +0700

    `refractor`: implement UUID trait in employee model

--- app/Models/EmployeeModel.php
+++ app/Models/EmployeeModel.php
@@ -4,14 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\WithUUID;
 
 class EmployeeModel extends Model
 {
-    use HasFactory;
+    use HasFactory, WithUUID;
 
     protected $table = 'employee';
-    protected $keyType = 'string';
-    public $incrementing = false;
 
     protected $fillable = [
         'name',
